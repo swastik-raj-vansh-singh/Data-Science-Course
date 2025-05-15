7b597c4e62ad53927e947db1b368f7a382bd365d
swastik-raj-vansh-singh <swastikrajvanshsingh0@gmail.com>
2025-05-16 03:07:08 +0530

    lerning about the dictionaries

---
+++ basic.py
@@ -0,0 +1,31 @@
+# creating a virtual environment:
+# e
+
+# dictionaries
+
+# students = {"name":['swastik','shruti'], "age" : [4,6]}
+# # print (students['name'][1])
+
+# students['gender'] = ["M","F"]
+# # print (students)
+
+# # looping through the dict
+# for key in students:
+#     print(key)
+
+# for value in students.values():
+#     print(value)
+
+# for key,value in students.items():
+#     print (f"{key} - {value}")
+
+s = "my name is swastik"
+words = s.split()
+# print(words)
+
+# count the frequency of workd
+dict = {}
+for word in words :
+    dict[word] = dict.get(word,0) +1
+
+print (dict)
