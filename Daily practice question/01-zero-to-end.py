289d240d8d754cb267ea958a9c58a0cdd34be6df
swastik-raj-vansh-singh <swastikrajvanshsingh0@gmail.com>
2025-05-19 03:18:33 +0530

    Day 1: Move Zeroes - Two Pointer Approach

---
+++ Daily practice question/01-zero-to-end.py
@@ -0,0 +1,20 @@
+def MovingZeroToTheLast(l):
+    fast = 0
+    slow = 0
+    n = len(l)
+    while(fast < n):
+        if l[fast] !=0 :
+            l[slow] = l[fast]
+            if slow != fast:
+                l[fast] = 0
+            slow += 1
+            fast +=1
+        else:
+            fast +=1
+    return l
+        
+
+array  = [0,1,0,3,12]
+array1 = [1,0,0,2,2]
+l = MovingZeroToTheLast(array1)
+print (l)
