56187ff5004aa97af333a8b5097c09cee8870fae
swastik-raj-vansh-singh <swastikrajvanshsingh0@gmail.com>
2025-05-20 01:49:31 +0530

    finding the piviot adding 2nd approach

--- Daily practice question/03-findingPivot.py
+++ Daily practice question/03-findingPivot.py
@@ -31,3 +31,15 @@ array = [0,0,0, 0]
 print (pivot(array))
 
 
+
+# add second and smaller approach 
+def pivot_index(nums):
+    total_sum = sum(nums)
+    left_sum = 0
+
+    for i in range(len(nums)):
+        if left_sum == total_sum - left_sum - nums[i]:
+            return i
+        left_sum += nums[i]
+
+    return -1
